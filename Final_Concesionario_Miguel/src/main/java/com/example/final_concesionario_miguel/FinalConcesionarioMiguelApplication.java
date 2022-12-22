333b6f96a213f7244c1968e8db86d67cca6b4cf4
Miguel Tapia <61064966+Migizito@users.noreply.github.com>
2022-12-22 15:48:06 -0400

    Commit change

---
+++ Final_Concesionario_Miguel/src/main/java/com/example/final_concesionario_miguel/FinalConcesionarioMiguelApplication.java
@@ -0,0 +1,13 @@
+package com.example.final_concesionario_miguel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalConcesionarioMiguelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalConcesionarioMiguelApplication.class, args);
+    }
+
+}
